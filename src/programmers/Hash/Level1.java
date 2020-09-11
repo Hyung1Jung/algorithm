b2dd08e80372ebd63530516c2e829fc08377b13b
Hyung1Jung <junghyungile@gmail.com>
2020-09-12 03:55:03 +0900

    Hash

---
+++ src/programmers/Hash/Level1.java
@@ -0,0 +1,10 @@
+package programmers.Hash;
+
+public class Level1 {
+
+    public static void main(String[] args) {
+        // write your code here
+
+        System.out.println("hello world");
+    }
+}
