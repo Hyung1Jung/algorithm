fa488fb31e6f23e62c51e216df388ec0b134c462
Hyung1Jung <junghyungile@gmail.com>
2020-09-12 05:24:34 +0900

    Hash

--- src/programmers/Hash/Level1.java
+++ src/programmers/Hash/Level1.java
@@ -1,10 +1,20 @@
 package programmers.Hash;
 
+import java.util.HashMap;
+
 public class Level1 {
 
-    public static void main(String[] args) {
-        // write your code here
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        HashMap<String, Integer> hm = new HashMap<>();
+        for (String player : participant) hm.put(player, hm.getOrDefault(player, 0) + 1);
+        for (String player : completion) hm.put(player, hm.get(player) - 1);
 
-        System.out.println("hello world");
+        for (String key : hm.keySet()) {
+            if (hm.get(key) != 0){
+                answer = key;
+            }
+        }
+        return answer;
     }
 }
