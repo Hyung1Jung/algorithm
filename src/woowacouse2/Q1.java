d48e471e18292233254457c98c8094022f775580
Hyung1Jung <junghyungile@gmail.com>
2020-09-14 07:33:35 +0900

    Q1

---
+++ src/woowacouse2/Q1.java
@@ -0,0 +1,19 @@
+package woowacouse2;
+
+public class Q1 {
+
+    public static void main(String[] args) {
+        int[] a=  {0, 0};
+        int[][] b = {{1000,0}};
+        System.out.println(solution(a, b, 1000));
+    }
+    public static int solution(int[] restaurant, int[][] riders, int k) {
+        int answer = 0;
+        for(int i=0; i<riders.length; i++) {
+            if(Math.pow(k, 2) >= Math.pow(restaurant[0]-riders[i][0],2)+Math.pow(restaurant[1]-riders[i][1],2)) {
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
