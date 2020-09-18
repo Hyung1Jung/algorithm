15d7f2bc411a14787578e90dcd3a53207f4e481c
Hyung1Jung <junghyungile@gmail.com>
2020-09-18 15:47:44 +0900

    Q3

---
+++ src/woowacouse2/Q3.java
@@ -0,0 +1,23 @@
+package woowacouse2;
+
+import java.util.Arrays;
+
+public class Q3 {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+    public int solution(int[] prices, int[] discounts) {
+        Arrays.sort(prices);
+        Arrays.sort(discounts);
+        int ans = 0;
+        int discountCnt = discounts.length;
+
+        for(int i=prices.length-1; i>=0; i--) {
+            ans+= discountCnt>=0 ? prices[i] * (100-discounts[discountCnt--]) / 100 : prices[i];
+
+        }
+        return ans;
+    }
+}
