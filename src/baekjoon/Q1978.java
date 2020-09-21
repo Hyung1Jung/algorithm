6860bb6a74df59feb8372992a55d5dd87a159004
Hyung1Jung <hyungiljung94@gmail.com>
2020-09-22 01:14:21 +0900

    소수찾기

--- src/baekjoon/Q1978.java
+++ src/baekjoon/Q1978.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
-
+// hyungil
 public class Q1978 {
 
     public static void main(String[] args) throws IOException {
