2e618ae584c0417d940ec9a76916f6fbb6c09784
Hyung1Jung <hyungiljung94@gmail.com>
2020-10-14 11:29:25 +0900

    update README.md

---
+++ src/practice.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class practice {
+
+    static int N;
+
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        //N = Integer.parseInt(st.nextToken());
+
+        //System.out.println(N);
+    }
+}
