2a77e4caabf8949ca0cfd89bbe4f71c289b59fe2
Hyung1Jung <hyungiljung94@gmail.com>
2020-10-21 20:54:45 +0900

    일곱 난장이

---
+++ src/baekjoon/Q2309.java
@@ -0,0 +1,38 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Q2309 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int[] a = new int[9];
+        int sum = 0;
+
+        for (int i = 0; i < 9; i++) {
+            int input = Integer.parseInt(br.readLine());
+            a[i] = input;
+            sum += input;
+        }
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = i + 1; j < 9; j++) {
+                if (sum - a[i] - a[j] == 100) {
+                    a[i] = 0;
+                    a[j] = 0;
+                    for (int s : a) {
+                        if (s != 0) {
+                            System.out.println(s);
+                        }
+
+                    }
+                }
+
+            }
+        }
+    }
+}
+
