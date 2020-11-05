481d831b902cf8dbf4f31fa05231a27bf56ee8ff
Hyung1Jung <hyungiljung94@gmail.com>
2020-11-06 03:12:07 +0900

    1기 문제3

--- src/woowacouse2/Q3_1th.java
+++ src/woowacouse2/Q3_1th.java
@@ -82,3 +82,33 @@ public class Q3_1th {
     }
 
 }
+/*
+방법 2
+
+    public static String solution(String word) {
+
+    static boolean isLowerCase(char c) { return 'a' <= c && c <= 'z'; }
+    static boolean isUpperCase(char c) { return 'A' <= c && c <= 'Z'; }
+
+    static boolean isAlphabetic(char c) {
+        return isLowerCase(c) || isUpperCase(c);
+    }
+
+    static char convert(char c) {
+        if (isLowerCase(c)) return (char)('z' - (c - 'a'));
+        if (isUpperCase(c)) return (char)('Z' - (c - 'A'));
+        return c;
+    }
+
+    public static String solution(String word) {
+        char[] a = word.toCharArray();
+        for (int i = 0; i < a.length; ++i)
+            a[i] = convert(a[i]);
+        return new String(a);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution("I love you"));
+    }
+}
+ */
