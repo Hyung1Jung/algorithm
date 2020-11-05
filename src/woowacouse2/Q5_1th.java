87ae0a9ff7dcffa2e7b1e41219d9b99135bdff28
Hyung1Jung <hyungiljung94@gmail.com>
2020-11-06 03:12:24 +0900

    1기 문제5

--- src/woowacouse2/Q5_1th.java
+++ src/woowacouse2/Q5_1th.java
@@ -55,3 +55,30 @@ public class Q5_1th {
         System.out.println(solution(33));
     }
 }
+
+/*
+방법2
+
+public static int solution(int number) {
+
+ static int count369(int number) {
+        int count = 0;
+        for (char c : String.valueOf(number).toCharArray())
+            if (c == '3' || c == '6' || c == '9')
+                ++count;
+        return count;
+    }
+
+    public static int solution2(int number) {
+        int sum = 0;
+        for (int i = 1; i <= number; ++i)
+            sum += count369(i);
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution2(13));
+        System.out.println(solution2(33));
+    }
+}
+ */
