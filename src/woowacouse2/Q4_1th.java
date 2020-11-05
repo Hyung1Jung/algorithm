466f7f4726d1adfef3df4ab4d7a158068ba018e2
Hyung1Jung <hyungiljung94@gmail.com>
2020-11-05 22:24:15 +0900

    1기 문제4

--- src/woowacouse2/Q4_1th.java
+++ src/woowacouse2/Q4_1th.java
@@ -2,24 +2,26 @@ package woowacouse2;
 
 public class Q4_1th {
 
-    public int solution(int[] pobi, int[] crong) {
+    public static int solution(int[] pobi, int[] crong) {
 
-        boolean page = validation(pobi, crong); // 책 예외상황 체크
-        if (!page) {
-            return -1;
-        }
+        // 페이지 예외상황 체크
+        boolean page = validation(pobi, crong);
+                if(!page) {
+                    return -1;
+                }
 
-        // 포비 점수 내기
+        // 포비 점수
         int pobi_sum_score = sum(pobi);
         int pobi_mul_score = mul(pobi);
 
-        int pobi_max_score = maxScore(pobi_sum_score, pobi_mul_score);
+        int pobi_max_score = maxScore(pobi_mul_score, pobi_sum_score);
 
         int crong_sum_score = sum(crong);
-        int crong_mul_score = sum(crong);
+        int crong_mul_score = mul(crong);
 
         int crong_max_score = maxScore(crong_mul_score, crong_sum_score);
 
+        // 점수 비교
         if (pobi_max_score > crong_max_score) {
             return 1;
         } else if (pobi_max_score < crong_max_score) {
@@ -29,66 +31,72 @@ public class Q4_1th {
         } else {
             return -1;
         }
+
     }
 
-        // 점수합산 // @param value 배열
-        static private int sum ( int[] value){
 
-            int val_1 = 0;
-            int val_2 = 0;
+    // 예외 상황 체크
+    static private boolean validation(int[] pobi, int[] crong) {
 
-            String[] array1 = String.valueOf(value[0]).split("");
-            for (String val : array1) {
-                val_1 = val_1 + Integer.valueOf(val);
-            }
+        if (1 != Math.abs(pobi[1] - pobi[0])) {
+            return false;
+        } else if (1 != Math.abs(crong[1] - crong[0])) {
+            return false;
+        } else return true;
+    }
 
-            String[] array2 = String.valueOf(value[1]).split("");
-            for (String val : array2) {
-                val_2 = val_2 + Integer.valueOf(val);
-            }
+    // 점수 합산
+    static private int sum( int[] value){
 
-            return maxScore(val_1, val_2);
+        int value1 = 0;
+        int value2 = 0;
+
+        String[] leftArray = String.valueOf(value[0]).split("");
+        for (String num : leftArray) {
+            value1 += Integer.valueOf(num);
         }
 
-        // 곱하기 // @param value 배열
-        static private int mul ( int value[]){
-
-            int val_1 = 1;
-            int val_2 = 1;
-
-            String[] array1 = String.valueOf(value[0]).split("");
-            for (String val : array1) {
-                val_1 = val_1 * Integer.valueOf(val);
-            }
-
-            String[] array2 = String.valueOf(value[1]).split("");
-            for (String val : array2) {
-                val_2 = val_2 * Integer.valueOf(val);
-            }
-
-            return maxScore(val_1, val_2);
+        String[] rightArray = String.valueOf(value[1]).split("");
+        for (String num : rightArray) {
+            value2 += Integer.valueOf(num);
         }
 
-        // 최고점수 출력
-        static private int maxScore ( int a, int b){
-            if (a > b) {
-                return a;
-            } else {
-                return b;
-            }
+        return maxScore(value1, value2);
+    }
+
+    // 점수 곱
+    static private int mul ( int[] value){
+
+        int value1 = 1;
+        int value2 = 1;
+
+        String[] leftArray = String.valueOf(value[0]).split("");
+        for (String num : leftArray) {
+            value1 *= Integer.valueOf(num);
         }
-        // 예외상황 체크
-        static private boolean validation ( int[] pobi, int[] crong){
 
-            boolean result = true;
+        String[] rightArray = String.valueOf(value[1]).split("");
+        for (String num : rightArray) {
+            value2 *= Integer.valueOf(num);
+        }
 
-            if (1 != Math.abs(pobi[0] - pobi[1])) {
-                return false;
-            } else if (1 != Math.abs(crong[0] - crong[1])) {
-                return false;
-            } else {
-                return result;
-            }
+        return maxScore(value1, value2);
+    }
+
+    // 최고점수 출력
+    static private int maxScore ( int value1, int value2){
+        if (value1 > value2) {
+            return value1;
+        } else {
+            return value2;
+        }
+    }
+
+
+    public static void main(String[] args) {
+        int[] arr1 = {35,37};
+        int[] arr2 = {56,57};
+        System.out.println(solution(arr1,arr2));
     }
 
 }
