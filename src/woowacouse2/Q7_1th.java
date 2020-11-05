e99028c6197614b869c06b66c168b4b046547ba4
Hyung1Jung <hyungiljung94@gmail.com>
2020-11-06 03:12:32 +0900

    1기 문제7

--- src/woowacouse2/Q7_1th.java
+++ src/woowacouse2/Q7_1th.java
@@ -85,3 +85,57 @@ public class Q7_1th {
         System.out.println(solution("zyelleyz"));
     }
 }
+
+/*
+방법2
+
+    public static String solution(String cryptogram){
+
+    static class MyStack {
+        char[] a = new char[10];
+        int count = 0;
+
+        public void push(char c) {
+            if (count >= a.length) expand();
+            a[count++] = c;
+        }
+
+        void expand() {
+            a = Arrays.copyOf(a, a.length * 2);
+        }
+
+        public char peek() {
+            return a[count - 1];
+        }
+
+        public char pop() {
+            return a[--count];
+        }
+
+        public int count() {
+            return count;
+        }
+
+        @Override
+        public String toString() {
+            return new String(a, 0, count);
+        }
+    }
+
+    public static String solution(String s) {
+        MyStack stack = new MyStack();
+        for (char c : s.toCharArray())
+            if (stack.count() > 0 && stack.peek() == c)
+                stack.pop();
+            else
+                stack.push(c);
+        return stack.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution("browoanoommnaon"));
+    }
+
+}
+
+ */
