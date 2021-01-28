118170372f972b66171ff04bbc25f8af26e7c5b4
Hyung1Jung <hyungiljung94@gmail.com>
2021-01-28 18:42:56 +0900

    feat : 프린터

---
+++ src/programmers/stack_and_queue/프린터.java
@@ -0,0 +1,41 @@
+package programmers.stack_and_queue;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class 프린터 {
+
+    public static int solution(int[] priorities, int location) {
+        int answer = 1;
+
+        PriorityQueue<Integer> pq = new PriorityQueue<Integer>(Collections.reverseOrder());
+
+        for (int i : priorities) {
+            pq.add(i);
+        }
+
+        while (!pq.isEmpty()) {
+            int size = priorities.length;
+            for (int i = 0; i < size; i++) {
+                if (priorities[i] == (int) pq.peek()) {
+                    if (i == location) {
+                        return answer;
+                    }
+                    pq.poll();
+                    answer++;
+                }
+            }
+        }
+            return answer;
+        }
+
+        public static void main (String[]args){
+
+            int[] priorities = {2, 1, 3, 2};
+            int location = 2;
+            int answer = solution(priorities, location);
+            System.out.println(answer);
+
+        }
+    }
