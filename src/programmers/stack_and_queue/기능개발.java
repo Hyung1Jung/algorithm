b45c6e619b3dcda21990e9e4a3313c4ef7aa3639
Hyung1Jung <hyungiljung94@gmail.com>
2021-02-07 20:40:44 +0900

    refactor : 줄간격 수정

---
+++ src/programmers/stack_and_queue/기능개발.java
@@ -0,0 +1,16 @@
+package programmers.stack_and_queue;
+
+public class 기능개발 {
+
+    public static int[] solution(int[] progresses, int[] speeds) {
+        int[] answer = {};
+        return answer;
+    }
+
+
+    public static void main(String[] args) {
+        int[] progresses = {93, 30, 55};
+        int[] speeds = {1, 30, 5};
+        System.out.println(solution(progresses, speeds));
+    }
+}
