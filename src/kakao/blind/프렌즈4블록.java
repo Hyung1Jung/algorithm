f2d13a77d14da9460ffe7c9a2786b3ecbe9e03ca
jeonghyeong-il <junghyungile@gmail.com>
2022-03-14 01:16:37 +0900

    refactor : 프렌즈4블록

--- src/kakao/blind/프렌즈4블록.java
+++ src/kakao/blind/프렌즈4블록.java
@@ -4,14 +4,15 @@ import java.util.Arrays;
 
 public class 프렌즈4블록 {
 
+    static int M, N;
+    static boolean check;
+
     public int solution(int m, int n, String[] board) {
         int answer = 0;
+        M = m;
+        N = n;
 
         char[][] map = new char[m][n];
-        char[][] tempMap;
-        char[][] checkMap = new char[m][n];
-
-        boolean check;
 
         for (int i = 0; i < m; ++i) {
             map[i] = board[i].toCharArray();
@@ -19,64 +20,78 @@ public class 프렌즈4블록 {
 
         do {
             check = false;
-            tempMap = deepCopy(map);
-
-            for (int i = 0; i < m - 1; ++i) {
-                for (int j = 0; j < n - 1; ++j) {
-                    char value = map[i][j];
-
-                    if ((map[i][j + 1] == value) && (map[i + 1][j] == value) && (map[i + 1][j + 1] == value)) {
-                        map[i][j] = ' ';
-                        map[i][j + 1] = ' ';
-                        map[i + 1][j] = ' ';
-                        map[i + 1][j + 1] = ' ';
-
-                        checkMap[i][j] = value;
-                        checkMap[i][j + 1] = value;
-                        checkMap[i + 1][j] = value;
-                        checkMap[i + 1][j + 1] = value;
-
-                    }
-
-                    if (value == ' ') {
-                        if ((map[i][j + 1] == ' ' || map[i][j + 1] == checkMap[i][j]) && (map[i + 1][j] == ' ' || map[i + 1][j] == checkMap[i][j]) && (map[i + 1][j + 1] == ' ' || map[i + 1][j + 1] == checkMap[i][j])) {
-                            map[i][j + 1] = ' ';
-                            map[i + 1][j] = ' ';
-                            map[i + 1][j + 1] = ' ';
-
-                            checkMap[i][j] = checkMap[i][j];
-                            checkMap[i][j + 1] = checkMap[i][j];
-                            checkMap[i + 1][j] = checkMap[i][j];
-                            checkMap[i + 1][j + 1] = checkMap[i][j];
-                        }
-                    }
-                }
-            }
-
-            answer = 0;
-
-            for (int i = 1; i < m; ++i) {
-                for (int j = 0; j < n; ++j) {
-
-                    if (map[i][j] == ' ') {
-                        map[i][j] = map[i - 1][j];
-                        map[i - 1][j] = ' ';
-                    }
-
-                    if (map[i - 1][j] == ' ') answer++;
-                    if (i == m - 1 && map[i][j] == ' ') answer++;
-
-                }
-            }
-
-            if (!Arrays.deepEquals(tempMap, map)) check = true;
-
+            removeBlock(map);
+            answer = mapSort(map);
         } while (check);
 
         return answer;
     }
 
-    char[][] deepCopy(char[][] original2) {
+    private void removeBlock(char[][] map) {
+        char[][] checkMap = new char[M][N];
+
+        for (int i = 0; i < M - 1; ++i) {
+            for (int j = 0; j < N - 1; ++j) {
+                char value = map[i][j];
+
+                if ((map[i][j + 1] == value) && (map[i + 1][j] == value) && (map[i + 1][j + 1] == value)) {
+                    map[i][j] = ' ';
+                    map[i][j + 1] = ' ';
+                    map[i + 1][j] = ' ';
+                    map[i + 1][j + 1] = ' ';
+
+                    checkMap[i][j] = value;
+                    checkMap[i][j + 1] = value;
+                    checkMap[i + 1][j] = value;
+                    checkMap[i + 1][j + 1] = value;
+
+                }
+
+                if (value == ' ') {
+                    if ((map[i][j + 1] == ' ' || map[i][j + 1] == checkMap[i][j]) && (map[i + 1][j] == ' ' || map[i + 1][j] == checkMap[i][j]) && (map[i + 1][j + 1] == ' ' || map[i + 1][j + 1] == checkMap[i][j])) {
+                        map[i][j + 1] = ' ';
+                        map[i + 1][j] = ' ';
+                        map[i + 1][j + 1] = ' ';
+
+                        checkMap[i][j] = checkMap[i][j];
+                        checkMap[i][j + 1] = checkMap[i][j];
+                        checkMap[i + 1][j] = checkMap[i][j];
+                        checkMap[i + 1][j + 1] = checkMap[i][j];
+                    }
+                }
+            }
+        }
+    }
+
+    private int mapSort(char[][] map) {
+        int count = 0;
+        char[][] tempMap = deepCopy(map);
+
+        for (int i = 1; i < M; ++i) {
+            for (int j = 0; j < N; ++j) {
+                if (map[i][j] == ' ') {
+                    map[i][j] = map[i - 1][j];
+                    map[i - 1][j] = ' ';
+                }
+
+                if (map[i - 1][j] == ' ') {
+                    count++;
+                }
+
+                if (i == M - 1 && map[i][j] == ' ') {
+                    count++;
+                }
+            }
+        }
+
+        if (!Arrays.deepEquals(tempMap, map)) {
+            check = true;
+        }
+
+        return count;
+    }
+
+    private char[][] deepCopy(char[][] original2) {
         if (original2 == null) return null;
         char[][] result = new char[original2.length][original2[0].length];
 
